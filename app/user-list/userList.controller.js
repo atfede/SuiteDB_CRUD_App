29744a99ce07e1d93644846f5c93673cd4bfb772
Federico Testoni <atfede@gmail.com>
2018-08-17 04:59:28 -0300

    Added repository

--- app/user-list/userList.controller.js
+++ app/user-list/userList.controller.js
@@ -5,8 +5,8 @@ angular.module('userListController', ['angularUtils.directives.dirPagination']) 
             '$scope',
             '$http',
             '$window',
-            '$cookies',
-            function ($scope, $http, $window, $cookies) {
+            '$rootScope',
+            function ($scope, $http, $window, $rootScope) {
                 $scope.AddModal = false;
                 $scope.EditModal = false;
                 $scope.DeleteModal = false;
@@ -14,8 +14,8 @@ angular.module('userListController', ['angularUtils.directives.dirPagination']) 
                 $scope.errorFirstname = false;
 
                 $scope.showAdd = function () {
-                    $scope.firstname = null;
-                    $scope.lastname = null;
+                    $scope.firstName = null;
+                    $scope.lastName = null;
                     $scope.address = null;
                     $scope.errorFirstname = false;
                     $scope.errorLastname = false;
@@ -24,27 +24,18 @@ angular.module('userListController', ['angularUtils.directives.dirPagination']) 
                 }
 
                 $scope.fetch = function () {
-                     $scope.members = $cookies.users;
 
-/*                    $scope.members = [
-                        {memid: 1, firstname: 'Fred', lastname: "Testoni", address: 'Silay City'},
-                        {memid: 2, firstname: 'Mark', lastname: "Lopez", address: 'Montevideo City'},
-                        {memid: 3, firstname: 'Tito', lastname: "Lopez", address: 'New York City'},
-                        {memid: 4, firstname: 'Triven', lastname: "Lopez", address: 'Sydney City'},
-                        {memid: 5, firstname: 'Fabi', lastname: "Lopez", address: 'Burundagui City'},
-                        {memid: 6, firstname: 'Maria Noel', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 7, firstname: 'Novick', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 8, firstname: 'John Lennon', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 9, firstname: 'Tatcher', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 10, firstname: 'Walker', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 11, firstname: 'O.J. Simpson', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 12, firstname: 'Marcos Wey', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 13, firstname: 'DJ Khaled', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 14, firstname: 'Neo', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 15, firstname: 'Morpheus', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 16, firstname: 'White Bunny', lastname: "Lopez", address: 'Silay City'},
-                        {memid: 17, firstname: 'Chris Novoselic', lastname: "Lopez", address: 'Silay City'}
-                    ];*/
+                    $scope.members = $rootScope.users;
+                    // $http({
+                    //     method: 'get',
+                    //     url: 'https://api.myjson.com/bins/1f6sdo',
+                    //     headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'}
+                    // }).then(function (response) {
+                    //         $scope.members = response.data.users;
+                    //     },
+                    //     function (response) {
+                    //         alert("Error retrieving users!");
+                    //     });
                 }
 
                 $scope.sort = function (keyname) {
--- app/user-registration/userRegistration.controller.js
+++ app/user-registration/userRegistration.controller.js
@@ -5,16 +5,22 @@ angular.module('userRegistrationController',
 )
     .controller('userRegistrationController',
         [
+            '$rootScope',
             '$scope',
             '$http',
             '$window',
-            '$cookies',
-            function ($scope, $http, $window, $cookies) {
+            '$location',
+            function ($rootScope, $scope, $http, $window, $location) {
 
-                $scope.user = {
+                var self = $scope;
+
+                self.uri = "";
+                self.users = [];
+                self.user = {
                     id: null,
                     firstName: '',
                     lastName: "",
+                    email: "",
                     phone: "",
                     address: '',
                     dateOfBirth: '',
@@ -22,60 +28,122 @@ angular.module('userRegistrationController',
                     gender: ''
                 };
 
-                $scope.AddModal = false;
-                $scope.EditModal = false;
-                $scope.DeleteModal = false;
+                self.AddModal = false;
+                self.EditModal = false;
+                self.DeleteModal = false;
 
-                $scope.errorFirstname = false;
+                self.errorFirstname = false;
 
-                $scope.showAdd = function () {
-                    $scope.firstname = null;
-                    $scope.lastname = null;
-                    $scope.address = null;
-                    $scope.errorFirstname = false;
-                    $scope.errorLastname = false;
-                    $scope.errorAddress = false;
-                    $scope.AddModal = true;
+                self.showAdd = function () {
+                    self.firstname = null;
+                    self.lastname = null;
+                    self.address = null;
+                    self.errorFirstname = false;
+                    self.errorLastname = false;
+                    self.errorAddress = false;
+                    self.AddModal = true;
                 }
 
-                $scope.init = function () {
-
-                    $cookies.users = [
-                        {memid: 1, firstname: 'Fred', lastname: "Testoni", address: 'Silay City'},
-                        {memid: 2, firstname: 'Mark', lastname: "Lopez", address: 'Montevideo City'},
-                        {memid: 3, firstname: 'Tito', lastname: "Lopez", address: 'New York City'}
-                    ];
-
-                    if ($cookies['users'] != undefined) {
-                        $scope.members = $cookies['users'];
+                self.init = function () {
+                    if (self.uri == "") {
+                        $http({
+                            method: 'POST',
+                            url: 'https://api.myjson.com/bins',
+                            data: "[]",
+                            headers: {'Content-Type': "application/json; charset=utf-8"}
+                        }).then(function (response) {
+                                self.uri = response.data.uri;
+                            },
+                            function (data) {
+                                alert("Error!");
+                            });
                     }
                 }
 
-                $scope.addUser = function () {
-                    var users = $cookies.users;
+                self.addUser = function () {
 
-                    var user = _.find(users, function (obj) {
-                        return (obj.lastname === $scope.user.lastName);
+                    var user = _.find(self.users, function (obj) {
+                        return (obj.lastName === self.user.lastName);
                     });
 
                     if (user == null) {
+                        var user = {
+                            id: self.getLastUserId(self.users),
+                            firstName: self.user.firstName,
+                            lastName: self.user.lastName,
+                            email: self.user.email,
+                            phone: self.user.phone,
+                            address: self.user.address,
+                            dateOfBirth: self.user.dateOfBirth,
+                            comments: self.user.comments,
+                            gender: self.user.gender
+                        };
 
-                        $cookies.users.push({
-                            id: users.length - 1,
-                            firstName: $scope.user.firstName,
-                            lastname: $scope.user.lastName,
-                            phone: $scope.user.phone,
-                            address: $scope.user.address,
-                            dateOfBirth: $scope.user.dateOfBirth,
-                            comments: $scope.user.comments,
-                            gender: $scope.user.gender
-                        });
+                        self.users.push(user);
+                        $rootScope.users = [];
 
-                        alert('User was created!');
+                        var updatedUsersJSON = JSON.stringify(self.users);
+
+                        self.updateUser(updatedUsersJSON);
+
                     }
                     else {
                         alert('Error: user already exists!');
                     }
                 }
+
+                self.getLastUserId = function (users) {
+                    if (users != undefined) {
+                        return users.length + 1;
+                    }
+                    else {
+                        return 1;
+                    }
+                }
+
+                self.updateUser = function (updatedUsers) {
+                    $.ajax({
+                        url: self.uri,
+                        type: "PUT",
+                        data: updatedUsers,
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data, textStatus, jqXHR) {
+                            var result = JSON.stringify(data);
+
+                            $rootScope.users.push(data);
+
+                            //$location.path("/userList");
+                        }
+                    });
+                }
+
+                /*$scope.updateUser = function (updatedUsers) {
+                 $.ajax({
+                 url: "https://api.myjson.com/bins",
+                 type: "POST",
+                 data: updatedUsers,
+                 contentType: "application/json; charset=utf-8",
+                 dataType: "json",
+                 success: function (data, textStatus, jqXHR) {
+
+                 var updatedData = JSON.stringify(updatedUsers);
+
+                 // do update
+                 $.ajax({
+                 url: data.uri,
+                 type: "PUT",
+                 data: updatedData,
+                 contentType: "application/json; charset=utf-8",
+                 dataType: "json",
+                 success: function (data, textStatus, jqXHR) {
+                 var json = JSON.stringify(data);
+
+                 alert(json);
+                 }
+                 });
+                 }
+                 });
+                 }*/
 
             }]);
